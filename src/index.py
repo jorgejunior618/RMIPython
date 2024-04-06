b6b5f9cc3f13ce01116619d8d0edcd3928fc5139
jorgejunior618 <jorgejunior618@gmail.com>
2024-04-06 11:47:38 -0300

    tratamento de erros e inicialização da definição do nome do jogador

--- src/index.py
+++ src/index.py
@@ -1,5 +1,13 @@
 from models.cliente import cliente
 from gui_resta_um import GuiRestaUm
+from gui_define_nome import GuiDefineNome
+
+class Jogador:
+  def __init__(self):
+    self.nome: str = None
+
+  def defineNome(self, nome: str):
+    self.nome = nome
 
 def mainApp():
   # Inicializando o socket do cliente do jogador
@@ -7,12 +15,21 @@ def mainApp():
   print("o arquivo é acusado como virus")
   print(";-;")
 
-  cli_id = cliente.registrarCliente("jorge")
+  jogador = Jogador()
+  try:
+    defineNome = GuiDefineNome(jogador.defineNome)
+    defineNome.iniciaAplicacao()
+  except Exception as e:
+    print("erro defineNome:", e)
+
+  if jogador.nome == None: return
+
+  cli_id = cliente.registrarCliente(jogador.nome)
   # Inicializando o objeto que instanciará o jogo
   try:
     meuJogo = GuiRestaUm(cliente, cliente_id=cli_id)
   except Exception as e:
-    print(e)
+    print("erro meuJogo:", e)
 
   # Inicializando a aplicação grafica do jogo
   meuJogo.iniciaAplicacao()
